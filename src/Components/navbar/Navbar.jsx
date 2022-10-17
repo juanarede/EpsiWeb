5fbba9f80b1e12fd7bae036a28923288ad6d5e11
loadEdar <eduardo.arede@gmail.com>
2022-10-17 15:29:12 -0300

    Se agrego form y controlador

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -50,9 +50,29 @@ const changeBackground = () => {
             <ul  className=" navbar-nav ms-auto me-4 my-3 my-lg-0">
                 <li  className="nav-item"><a class="nav-link me-lg-3" href="#features">Our Service</a></li>
                 <li  className="nav-item"><a class="nav-link me-lg-3" href="#download">About Us</a></li>
-                <li  className="nav-item"><a class="nav-link me-lg-3" href="#">Let's talk</a></li>
+                <li  className="nav-item"><a class="nav-link me-lg-3" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Let's talk</a></li>
             </ul>
-           
+            
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Messages</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className='modal-body'>
+       <form action="" method='POST'>
+        <div className='mb-3'>
+          <input className='form-control' type="email" id='email' name="email" placeholder='Email' />
+          <textarea className='form-control' name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
+          <input className='btn' type="submit" value="Send Message" name='btn-enviar'/>
+        </div>
+       </form>
+      </div>
+    </div>
+  </div>
+</div>
+
         </div>
     </div>
 
