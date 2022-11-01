46972b530e7d5979f5b4512b722bd59f38d7ebd6
Juan Arede <85207414+juanarede@users.noreply.github.com>
2022-11-01 18:43:43 -0300

    redes sociales agregado

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -12,7 +12,14 @@ function Footer() {
        
         <footer className="bg-black text-center py-5">
             <div className="container px-5">
-                <div className="text-white-50 small">
+            <div className="text-center text-white font-alt ">
+                 Seguinos en Nuestras Redes!!
+                 
+            </div>
+            <i style={{width:"2rem", color:"#fff",paddingTop:"0.3rem"}} class="fa-brands fa-facebook"></i>
+            <i style={{width:"2rem", color:"#fff",paddingTop:"0.3rem"}} class="fa-brands fa-instagram"></i>
+            <i style={{width:"2rem", color:"#fff",paddingTop:"0.3rem"}} class="fa-brands fa-linkedin"></i>
+                <div className="text-white-50 small d-flex justify-content-start pt-4 col-xs-12">
                     <div className="mb-2">&copy; EpsiWeb 2022. All Rights Reserved.</div>
                    
                 </div>
