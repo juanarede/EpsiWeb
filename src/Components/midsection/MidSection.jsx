40c174c1db544efab8f263647885d1245cb5928b
loadEdar <eduardo.arede@gmail.com>
2022-11-26 23:48:53 -0300

    Contenido DB

--- src/Components/midsection/MidSection.jsx
+++ src/Components/midsection/MidSection.jsx
@@ -4,12 +4,25 @@ import './midsection.css'
 
 import Aos from 'aos'
 import "aos/dist/aos.css"
+import Axios from "axios";
+import { useState } from "react";
+
 function MidSection() {
+
+  const[list, setList]=useState([]);
   
   useEffect(() => {
-    Aos.init({duration: 2000})
+    Aos.init({duration: 2000});
+    Axios({url: "http://localhost/EpsiWeb/api/contact/midsection.php"})
+    .then((response)=>{
+      setList(response.data.midsection[0]);
+      
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
  
- }, []);
+ }, [setList]);
   return (
     <>
       <div className='mid-section'>
@@ -17,7 +30,7 @@ function MidSection() {
             <div className="container px-5">
                 <div className="row gx-5 justify-content-center">
                     <div className="col-xl-8 text-mid">
-                        <div data-aos="fade-right" className="h2 fs-1 text-white mb-4 ">"Nuestro compromiso, es poder acompañarte y transformar tus ideas en algo maravilloso y dinámico"</div>
+                        <div data-aos="fade-right" className="h2 fs-1 text-white mb-4 ">{list.parrafo}</div>
                         <img className='logomid' src={logowithe} alt="EpsiWeb-Logo"  title='EpsiWeb-Mid'/>
                     </div>
                 </div>
